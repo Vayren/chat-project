38505db420f062bbd20b0ee82f5f1537703fc84f
DmytroNakonechnyi <vayren0965@gmail.com>
2020-10-12 15:12:06 +0300

    build project

---
+++ build/precache-manifest.0e84819e6a47e69fa38c638e70aad5d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf3c142646934268a7220dade1d1e5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15791b96b98a89640896",
+    "url": "/static/css/main.9cb7495d.chunk.css"
+  },
+  {
+    "revision": "7316430249f2c7846e18",
+    "url": "/static/js/2.52f683b5.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/static/js/2.52f683b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15791b96b98a89640896",
+    "url": "/static/js/main.a978111e.chunk.js"
+  },
+  {
+    "revision": "9bc2349bfbe76e87395f",
+    "url": "/static/js/runtime-main.e5afff51.js"
+  }
+]);
